c9f94a85f4d0c611739ac4afaf090ea9243056d1
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2024-12-11 22:04:33 -0600

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
     integrations: [mdx(), sitemap(), tailwind()],
+    site: 'https://eduardoconx.github.io/',
+    base: '/blog-con-x/',
 });
