6a802c6e563c1a4e8486985718dcb1c5b3496cfc
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2025-11-24 21:43:12 -0600

    Nuke whole project, start over again

--- src/consts.ts
+++
@@ -1,6 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Blog Con X';
-export const SITE_DESCRIPTION = 'Small blog to share interesting things about software development.';
-export const SUBDOMAIN = 'blog-con-x/';
