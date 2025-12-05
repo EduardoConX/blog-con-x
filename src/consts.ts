04e20436ea4fd6c50a041d42fcf6c59a1dc69a91
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2025-12-04 22:16:19 -0600

    Back to home in header

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,4 @@
 export const SITE_TITLE = "Blog Con X";
 export const SITE_DESCRIPTION =
   "Small blog to share interesting things about software development.";
+export const SUBDOMAIN = 'blog-con-x/';
