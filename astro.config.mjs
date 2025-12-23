4b37d94b21a9925f1f23ccd5865210150f0447f7
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2025-12-23 17:19:40 -0600

    Vue installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), vue()],
   site: "https://eduardoconx.github.io",
   base: "/blog-con-x",
 });
