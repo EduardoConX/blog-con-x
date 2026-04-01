6406685c4129434be86199464896d399437c9822
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2026-03-31 19:43:02 -0600

    Nuke whole project

--- src/consts.ts
+++
@@ -1,4 +0,0 @@
-export const SITE_TITLE = "Blog Con X";
-export const SITE_DESCRIPTION =
-  "Small blog to share interesting things about software development.";
-export const SUBDOMAIN = 'blog-con-x/';
--- src/interfaces/Post.ts
+++
@@ -1,9 +0,0 @@
-export interface Post {
-  title: string;
-  description: string;
-  pubDate: Date;
-  slug: string;
-  category: {
-    id: string;
-  };
-}
