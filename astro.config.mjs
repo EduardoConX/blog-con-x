4dfba336f910d9e4b05391e02e6ef6be66bd79b8
Eduardo Pacheco <eduardopachecoquintanar@gmail.com>
2026-09-25 17:12:53 -0600

    Squash dev into main

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,20 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
 
-import mdx from "@astrojs/mdx";
+import mdx from '@astrojs/mdx';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
-import vue from "@astrojs/vue";
+import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [tailwindcss()]
   },
+
   integrations: [mdx(), sitemap(), vue()],
   site: "https://eduardoconx.github.io",
   base: "/blog-con-x",
